736b4930adddb88255eeed11ae6d0e5de244835a
eileenwang77 <wangew64@google.com>
2025-05-16 11:59:30 +0800

    l4-3-jinja2

--- lesson4/index.py
+++ lesson4/index.py
@@ -1,15 +1,16 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-# def hello_world():
-    # return "<p>Hello, World!</p>"
 def index():
-    return "<h1>Hello, Flask!</h1><p>這是我的第一頁</p>"
+    # return render_template("index.html")
+    return render_template("index.html.jinja2")
+
 @app.route("/user")
 def user():
-    return "<h1>user! </h1><p>這是我的第2頁</p>"
+    return "<h1>user!</h1><p>這是我的第2頁</p>"
+
 @app.route("/product")
 def product():
-    return "<h1>product! </h1><p>這是我的第3頁</p>"
+    return "<h1>product!</h1><p>這是我的第3頁</p>"
